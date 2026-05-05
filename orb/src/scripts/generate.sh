7b4159fdb569085ec107ee630bc490ac2b1718e6
Jeremiah Russell <jerry@jrussell.ie>
2026-05-05 14:37:04 +0100

    feat: wire orb generation via gen-circleci-orb init v0.0.10
    Generated with:
      gen-circleci-orb init \
        --binary gen-orb-mcp \
        --public-orb-namespace jerus-org \
        --docker-namespace jerusdp \
        --build-workflow validation \
        --release-workflow release \
        --requires-job toolkit/common_tests \
        --release-after-job release-gen-orb-mcp
    
    No manual fixes required.
    
    Signed-off-by: Jeremiah Russell <jerry@jrussell.ie>

---
+++ orb/src/scripts/generate.sh
@@ -0,0 +1,10 @@
+gen-orb-mcp generate \
+  --orb-path "<< parameters.orb_path >>" \
+  <<# parameters.output >>--output "<< parameters.output >>"<</ parameters.output >> \
+  <<# parameters.format >>--format "<< parameters.format >>"<</ parameters.format >> \
+  <<# parameters.name >>--name "<< parameters.name >>"<</ parameters.name >> \
+  <<# parameters.version >>--version "<< parameters.version >>"<</ parameters.version >> \
+  <<# parameters.force >>--force<</ parameters.force >> \
+  <<# parameters.migrations >>--migrations "<< parameters.migrations >>"<</ parameters.migrations >> \
+  <<# parameters.prior_versions >>--prior-versions "<< parameters.prior_versions >>"<</ parameters.prior_versions >> \
+  <<# parameters.tag_prefix >>--tag-prefix "<< parameters.tag_prefix >>"<</ parameters.tag_prefix >>
