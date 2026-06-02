1c3fa6a762159f5612806ade8057521baf915ae2
Jeremiah Russell <jerry@jrussell.ie>
2026-06-02 16:41:22 +0100

    chore: strip orb source and CI wiring for v0.0.38 init
    Signed-off-by: Jeremiah Russell <jerry@jrussell.ie>

--- orb/src/scripts/diff.sh
+++
@@ -1,6 +0,0 @@
-set -- gen-orb-mcp diff
-set -- "$@" --current "${CURRENT}"
-set -- "$@" --previous "${PREVIOUS}"
-set -- "$@" --since-version "${SINCE_VERSION}"
-[ -n "${OUTPUT:-}" ] && set -- "$@" --output "${OUTPUT}"
-"$@"
